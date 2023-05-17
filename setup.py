16ba463315602ec8cdc10c8747a75888d974f836
Kelsey Beyer <kelseymok@gmail.com>
2023-05-17 13:49:52 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os.path import basename
+from os.path import splitext
+from glob import glob
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='generic_consumer',
+    version='0.1.0',
+    description='Generic Kakfa Consumer',
+    author='Kelsey Mok',
+    author_email='kelseymok@gmail.com',
+    url='https://github.com/data-derp/generic_consumer',
+    packages=find_packages('src', exclude=('tests')),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    install_requires=required
+)
